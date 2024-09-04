09405e817e2c82776b49f79a33fa183fedc2eb16
losef <losef@DESKTOP-OFDT2BB>
2024-09-04 22:20:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bank_of_Khabib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
