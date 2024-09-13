8d96b7e55b20905822be08ce71d10477daa27b31
losef <losef@deck>
2024-09-14 00:01:21 +0300

    #пк

--- StartUp.cs
+++ StartUp.cs
@@ -5,7 +5,7 @@
         internal static DataBaseMock DataBaseMock { get; private set; }
         internal static User? CurrentUser { get; private set; }
         public static void Start()
-        {        
+        {
 
             DataBaseMock = new DataBaseMock();
 
@@ -15,113 +15,99 @@
             {
                 Console.WriteLine("Доступные пользователи:");
                 foreach (var us in DataBaseMock.Users)
-                {                    
+                {
                     Console.WriteLine($"Логин: {us.Login} Пароль: {us.Password}");
                 }
 
                 Console.WriteLine();
                 Console.Write("Введите логин или 1 для выхода: ");
-                var login = Console.ReadLine();
-
+                string login = Console.ReadLine();
+                string pass;
                 if (login == "1")
                 {
                     checkLogin = true;
+                    Console.WriteLine("Всего хорошего");
+                    return;
+                }
+                else
+                {
+                    Console.Write("Введите пароль: ");
+                    pass = Console.ReadLine();
+                }
+                foreach (var us in DataBaseMock.Users)
+                {
+                    if (login == us.Login && us.Password == pass)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Пользователь найден");
+                        Console.WriteLine();
+                        checkLogin = true;
+                        CurrentUser = us;
+                        break;
+                    }
+                }
+
+                if (CurrentUser == null)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Не верный логин или пароль");
                     Console.WriteLine();
                 }
-                else 
+                else
                 {
-                    foreach (var us in DataBaseMock.Users)
+                    Console.WriteLine($"{CurrentUser.Name}, Добро пожаловать в банковскую систему Хабибуллы!");
+                    bool exit = true;
+                    while (exit)
                     {
-                        if (login == us.Login)
+                        Console.WriteLine("Выбери 1.Проверить счета 2. 3.Сменить пользователя 4.Выход");
+                        if (int.TryParse(Console.ReadLine(), out int imput))
                         {
-                            Console.WriteLine("Пользователь найден");
-                            checkLogin = true;
-
-                            bool checkPass = false;
-
-                            while (!checkPass)
+                            switch (imput)
                             {
-                                Console.Write("Введите пароль или 1 для выхода: ");
-                                var pass = Console.ReadLine();
-
-                                if (pass == us.Password)
-                                {
-                                    Console.WriteLine("Пароль верный!");
-                                    CurrentUser = us;
-                                    checkPass = true;
-                                }
-                                else if (pass == "1")
-                                {
-                                    checkPass = true;
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Прароль не верный");
-                                }
+                                case 1:
+                                    var bills = DataBaseMock.Banks.SelectMany(b => b.GetBills(CurrentUser));
+                                    Console.WriteLine($"Все счета:");
+                                    Console.WriteLine($"{string.Join('\n', bills)}");
+                                    break;
+                                case 2: Console.WriteLine("т"); break;
+                                case 3:
+                                    Console.WriteLine("Всего хорошего");
+                                    exit = false;
+                                    checkLogin = false;
+                                    CurrentUser = null;
+                                    break;
+                                case 4:
+                                    Console.WriteLine("");
+                                    exit = false;
+                                    break;
+                                default: Console.WriteLine("выбери из четырёх цифр"); break;
                             }
-                            break;
+
                         }
-                    }
-                    if (CurrentUser == null)
-                    {
-                        Console.WriteLine("Пользователь не найден");
-                        Console.WriteLine();
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{CurrentUser.Name}, Добро пожаловать в банковскую систему Хабибуллы!");
-                        bool exit = true;
-                        while (exit)
+                        else
                         {
-                            Console.WriteLine("Выбери 1.Проверить счета 2. 3.Сменить пользователя 4.Выход");
-                            if (int.TryParse(Console.ReadLine(), out int imput))
-                            {
-                                switch (imput)
-                                {
-                                    case 1: 
-                                        var bills = DataBaseMock.Banks.SelectMany(b => b.GetBills(CurrentUser));
-                                        Console.WriteLine($"Все счета:");
-                                        Console.WriteLine($"{string.Join('\n', bills)}");
-                                        break;
-                                    case 2: Console.WriteLine("т"); break;
-                                    case 3: 
-                                        Console.WriteLine("Всего хорошего"); 
-                                        exit = false; 
-                                        checkLogin = false; 
-                                        CurrentUser = null; 
-                                        break;
-                                    case 4: 
-                                        Console.WriteLine(""); 
-                                        exit = false; 
-                                        break;
-                                    default: Console.WriteLine("выбери из четырёх цифр"); break;
-                                }
-
-                            }
-                            else
-                            {
-                                Console.WriteLine("выбери из четырёх цифр");
-                            }
+                            Console.WriteLine("выбери из четырёх цифр");
                         }
                     }
                 }
             }
-
-
-
-            //var u = DataBaseMock.Users.FirstOrDefault(t => t.Login == login);
-
-            //if (u != null)
-            //{
-            //    Console.WriteLine(
-            //    CurrentUser = u;
-            //}
-            //else
-            //{
-            //    Console.WriteLine("такого нет");
-            //}
-
-
         }
+
+
+
+        //var u = DataBaseMock.Users.FirstOrDefault(t => t.Login == login);
+
+        //if (u != null)
+        //{
+        //    Console.WriteLine(
+        //    CurrentUser = u;
+        //}
+        //else
+        //{
+        //    Console.WriteLine("такого нет");
+        //}
+
+
     }
 }
+
